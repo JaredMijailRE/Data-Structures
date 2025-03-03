559ae3008eda3f47babb305f89203cd803e9880c
JaredMijailRE <jramirezes@unal.edu.co>
2025-03-02 21:11:55 -0500

    taller final

---
+++ Analisys/structures/unionFind.py
@@ -0,0 +1,36 @@
+class UnionFind:
+    def __init__(self, n, use_path_compression=True):
+        # Inicializa n conjuntos; cada elemento es su propio representante
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n  # Rango para la unión por tamaño
+        self.use_path_compression = use_path_compression  # Bandera para path compression
+        
+    def find(self, x):
+        # Retorna el representante del conjunto al que pertenece x
+        if self.parent[x] != x:
+            if self.use_path_compression:
+                # Compresión de caminos: actualiza el padre de x para apuntar al representante
+                self.parent[x] = self.find(self.parent[x])
+            else:
+                return self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        # Une los conjuntos que contienen x e y
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return  # Ya están unidos
+
+        # Unión por rango: el árbol de menor rango se cuelga del de mayor rango
+        if self.rank[rootX] < self.rank[rootY]:
+            self.parent[rootX] = rootY
+        elif self.rank[rootX] > self.rank[rootY]:
+            self.parent[rootY] = rootX
+        else:
+            self.parent[rootY] = rootX
+            self.rank[rootX] += 1
+            
+    def connected(self, x, y):
+        # Retorna True si x e y están en el mismo conjunto
+        return self.find(x) == self.find(y)
